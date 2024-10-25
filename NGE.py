de4b89d6ffc3a3d8a7142e26a4a532cd011bc739
PuvaneshwaranV <vpuvaneshwaran5@gmail.com>
2024-10-25 14:53:37 +0530

    git push origin main

---
+++ NGE.py
@@ -0,0 +1,26 @@
+arr=[6,3,9,10,8,2,1,15,7]
+arr1=['_']*len(arr)
+i=0
+while(i<len(arr)):
+    k=0
+    j=0
+    m=0
+    index=0
+    while(j<len(arr)):
+        if(arr[i]<arr[j]):
+            t=arr[j]-arr[i]
+            if(m==0):
+                k=t
+                index=j
+                m+=1
+            elif(k>t):
+                k=t
+                index=j
+                m+=1
+        j+=1
+    if(m!=0):
+        arr1[i]=arr[index]
+    else:
+        arr1[i]="_"
+    i+=1
+print(arr1)
