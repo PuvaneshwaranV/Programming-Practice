3bb6a51e2136d1287f1e46c33aa0f921a1ab282b
PuvaneshwaranV <vpuvaneshwaran5@gmail.com>
2024-10-25 15:55:30 +0530

    commit -m "Modified arraddsame"

--- arraddsame.py
+++ arraddsame.py
@@ -1,18 +1,17 @@
 arr=[2,2,3,3,4,4,5,5]
 i=0
 k=0
-s=[]
+s=[0]*len(arr)
 while i<len(arr):
     if(arr[i]==0):
-        k+=1
+        pass
     elif((i+1)<len(arr) and arr[i]==arr[i+1]):
         arr[i]+=arr[i]
-        s.append(arr[i])
+        s[k]=arr[i]
         arr[i+1]=0
+        k+=1
     else:
-        s.append(arr[i])
+        s[k]=arr[i]
+        k+=1
     i+=1
-while(k>0):
-    s.append(0)
-    k-=1
 print(s)
