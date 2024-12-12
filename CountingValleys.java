cef721fe025ef71da795c618634dd5ed47564601
PuvaneshwaranV <vpuvaneshwaran5@gmail.com>
2024-12-12 21:06:28 +0530

    commit -m "CountingValleys"

---
+++ CountingValleys.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int countingValleys(int steps, String path) {
+        int k=0 , ans=0;
+    for(int i=0;i<steps;i++)
+    {
+        while(k<0)
+        {
+            if(k==-1 && path.charAt(i)=='U')
+                ans++;
+            if(path.charAt(i++)=='U')
+                k++;
+            else
+                k--;
+        }
+        if(i<steps && path.charAt(i)=='U')
+                k++;
+        else if(i<steps)
+                k--;
+    }
+        return ans;
+    }
+
+}
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
