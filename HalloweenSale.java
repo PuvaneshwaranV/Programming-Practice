d80b163521e6cfea1a24572f7b90bccef214ccc2
PuvaneshwaranV <vpuvaneshwaran5@gmail.com>
2025-02-16 16:14:48 +0530

    commit -m "HalloweenSale"

---
+++ HalloweenSale.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int howManyGames(int p, int d, int m, int s) {
+        int c=0;
+    
+   while(s>=p)
+   {
+        s-=p;
+        if(p-d>m)
+         p-=d;
+        else
+            p=m;
+        c++;
+    }
+    System.out.println(s+ " "+p);
+   
+   return c;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int p = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        int m = Integer.parseInt(firstMultipleInput[2]);
+
+        int s = Integer.parseInt(firstMultipleInput[3]);
+
+        int answer = Result.howManyGames(p, d, m, s);
+
+        bufferedWriter.write(String.valueOf(answer));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
