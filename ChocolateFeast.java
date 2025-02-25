43c75d2efd9a7bdd885f14b42995a0b2be19c456
PuvaneshwaranV <vpuvaneshwaran5@gmail.com>
2025-02-25 15:48:06 +0530

    commit -m "ChocolateFeast"

---
+++ ChocolateFeast.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int chocolateFeast(int n, int c, int m) {
+        int ans=0,rem=0,e=0;
+        ans=n/c;
+        e=ans;
+        rem=n%c;
+    while(e >= m)
+    {
+        rem=e%m;
+        e=e/m;
+        ans+=e;
+        e+=rem;
+    }
+    return ans;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int c = Integer.parseInt(firstMultipleInput[1]);
+
+                int m = Integer.parseInt(firstMultipleInput[2]);
+
+                int result = Result.chocolateFeast(n, c, m);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
